c635d0408c65dba4f57aeefca353fc665c4fdd1f
abhimanyu <abhimanew2000@gmail.com>
2024-03-12 00:02:33 +0530

    modified citymap

--- src/Utils/urls.js
+++ src/Utils/urls.js
@@ -1,3 +1,3 @@
 // export const baseUrl = "http://13.51.198.219/";
-// export const baseUrl = "https://abhimanew.live/"
-export const baseUrl = 'http://127.0.0.1:8000/';
+export const baseUrl = "https://abhimanew.live/"
+// export const baseUrl = 'http://127.0.0.1:8000/';
