e0f97a63082417a5ad5c4de15e5aadeb86599e73
abhimanyu <abhimanew2000@gmail.com>
2024-03-11 12:41:46 +0530

    modified userdropdown comp

--- src/components/user/UserDropDown.jsx
+++ src/components/user/UserDropDown.jsx
@@ -39,10 +39,9 @@ const handleLogout = () => {
       <div class="absolute right-0 -top-4 z-10 w-56 origin-top-right overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
         <div class=" " role="none">
           <a href="#" class="text-gray-700 block px-4 py-2 text-sm hover:bg-blue-600 hover:text-white" role="menuitem" tabindex="-1" id="menu-item-0">Account settings</a>
-          <a href="#" class="text-gray-700 block px-4 py-2 text-sm hover:bg-blue-600 hover:text-white" role="menuitem" tabindex="-1" id="menu-item-0">Notification
-          </a>
+          <Link to="/notification" class="text-gray-700 block px-4 py-2 text-sm hover:bg-blue-600 hover:text-white" role="menuitem" tabindex="-1" id="menu-item-0">Notification
+          </Link>
           <Link className='hover:bg-blue-600 text-gray-700 block px-4 py-2 text-sm hover:text-white' to="/user/profile" > Profile</Link>
-          <Link class="text-gray-700 block px-4 py-2 text-sm hover:bg-blue-600 hover:text-white" role="menuitem" tabindex="-1" id="menu-item-2" to="/inbox">Chats</Link>
           <a  onClick={handleLogout} class="text-gray-700 block px-4 py-2 text-sm hover:bg-blue-600 hover:text-white" role="menuitem" tabindex="-1" id="menu-item-2">Signout</a>
 
         </div>
